5a37fb64899d22b31f0052cb4db171d59242dbf8
Muarem <s.muarem10@gmail.com>
2024-02-04 22:48:54 +0400

    sign in, sign up processes completed

--- app/src/main/java/com/imsoft/kotlinpostsharingapp/MainActivity.kt
+++ app/src/main/java/com/imsoft/kotlinpostsharingapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.imsoft.kotlinpostsharingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -11,19 +13,69 @@ import com.imsoft.kotlinpostsharingapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth
+
+        val currentUser = auth.currentUser
+
+        if (currentUser != null) {
+            val intent = Intent(this@MainActivity, GetPostActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
+
+
 
     fun signInBtnClick(view: View) {
+        val email = binding.emailText.text.toString()
+        val password = binding.passwordText.text.toString()
 
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener {
+                    val intent = Intent(this@MainActivity, GetPostActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                .addOnFailureListener { exception ->
+                    Toast.makeText(this@MainActivity, exception.localizedMessage, Toast.LENGTH_LONG).show()
+                }
+        } else if (email == "" && password.isNotEmpty()) {
+            Toast.makeText(this@MainActivity, "Enter email!", Toast.LENGTH_LONG).show()
+        } else if (email.isNotEmpty() && password == "") {
+            Toast.makeText(this@MainActivity, "Enter password!", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this@MainActivity, "Enter email and password!", Toast.LENGTH_LONG).show()
+        }
     }
     fun signUpBtnClick(view: View) {
+        val email = binding.emailText.text.toString()
+        val password = binding.passwordText.text.toString()
 
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener {
+                    val intent = Intent(this@MainActivity, GetPostActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                .addOnFailureListener { exception ->
+                    Toast.makeText(this@MainActivity, exception.localizedMessage, Toast.LENGTH_LONG).show()
+                }
+        } else if (email == "" && password.isNotEmpty()) {
+            Toast.makeText(this@MainActivity, "Enter email!", Toast.LENGTH_LONG).show()
+        } else if (password == "" && email.isNotEmpty()) {
+            Toast.makeText(this@MainActivity, "Enter password!", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this@MainActivity, "Enter email and password!", Toast.LENGTH_LONG).show()
+        }
 
     }
 }
