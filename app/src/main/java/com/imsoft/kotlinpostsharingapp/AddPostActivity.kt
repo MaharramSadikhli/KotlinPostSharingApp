ca4166dd3b91197c58dd6d8ac20b51fd3deddc65
Muarem <s.muarem10@gmail.com>
2024-02-05 19:42:56 +0400

    user permissions completed

--- app/src/main/java/com/imsoft/kotlinpostsharingapp/AddPostActivity.kt
+++ app/src/main/java/com/imsoft/kotlinpostsharingapp/AddPostActivity.kt
@@ -1,19 +1,108 @@
 package com.imsoft.kotlinpostsharingapp
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import com.imsoft.kotlinpostsharingapp.databinding.ActivityAddPostBinding
 
 class AddPostActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddPostBinding
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    private var selectedImage: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddPostBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        registerLauncher()
     }
 
     fun postBtnClick(view: View) {}
-    fun selectImage(view: View) {}
+    fun selectImage(view: View) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this@AddPostActivity, Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED) {
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this@AddPostActivity, Manifest.permission.READ_MEDIA_IMAGES)) {
+                    Snackbar.make(view, "Permission Needed For Galery!", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("OK!") {
+                            permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                        }
+                        .show()
+                } else {
+                    permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                }
+
+            } else {
+                val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intent)
+            }
+        } else {
+            if (ContextCompat.checkSelfPermission(this@AddPostActivity, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this@AddPostActivity, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    Snackbar.make(view, "Permission Needed For Galery!", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("OK!") {
+                            permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        }
+                        .show()
+                } else {
+                    permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+
+            } else {
+                val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intent)
+            }
+        }
+
+    }
+
+    private fun registerLauncher() {
+
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result->
+
+            if (result.resultCode == RESULT_OK) {
+                val intent = result.data
+
+                if (intent != null) {
+                    selectedImage = intent.data
+                    selectedImage?. let {
+                        binding.imageView.setImageURI(it)
+                    }
+                }
+            }
+
+        }
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { result->
+
+            if (result) {
+
+                val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intent)
+
+            } else {
+                Toast.makeText(this@AddPostActivity, "Permission Needed!", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+    }
+
 }
